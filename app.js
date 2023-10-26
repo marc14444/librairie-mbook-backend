24b519aa7ce284019f4b08b4b5d8bd0980049234
Marc Harden <Marc_Harden@192.168.50.53>
2023-10-26 16:42:15 +0000

    ok

---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from "url";
+import {config} from "dotenv";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import userRouter from "./routes/users.js";
+import articleRouter from "./routes/articles.js"
+import {connectDb} from "./baseDEdonnees/db.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+config()
+connectDb()
+
+
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/api/user", userRouter);
+app.use("/api/article", articleRouter);
+connectDb()
+.then(()=>{
+    app.listen(port, () => console.log(`le serveur a bien demarrer sur http://localhost:${port}`));
+}).catch(()=>{
+    console.log("ok");
+})
---
+++ models/articles.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose"
+
+const userArticles = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    userId: {
+        type:Schema.Types.ObjectId,
+        ref:'user'
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    prix: {
+        type: Number,
+        required: true,
+    }
+
+})
+
+export default model("Articles", userArticles)
